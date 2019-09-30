adfeed3661e246872f4f0f5939c2fb9b970cca3e
Jennifer Du <jenniferdu97@gmail.com>
2019-09-30 12:36:23 +0100

    Initial commit
    Basic barebones of a chrome extension
    No real functionality implemented yet

---
+++ bg.js
@@ -0,0 +1,17 @@
+chrome.runtime.onInstalled.addListener(function(){
+
+  //check that the page it is being used on is an actual web page (http/https)
+  var checkPageValid= {
+    conditions:[
+      new chrome.declarativeContent.PageStateMatcher({
+        pageUrl:{schemes:['http','https']}
+      })
+    ],
+    actions:[new chrome.declarativeContent.ShowPageAction()]
+  };
+
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function(){
+    chrome.declarativeContent.onPageChanged.addRules([checkPageValid]);
+  });
+
+});
