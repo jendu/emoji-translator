0e719ff62fc87e54926b7c695ce9857dba9fc282
Jennifer Du <jenniferdu97@gmail.com>
2019-10-21 16:56:49 +0100

    Parses json file and stores as variable
    Replaced emojiDict with new one (better formatting, using unicode point as keys),
    added emojiDict to web_accessible_resources perms,
    started a stub to parse json
    
    Plan is to now find emojis on webpage, convert to unicode point, match to json library, and replace with description

--- scripts/replace.js
+++ scripts/replace.js
@@ -1,6 +1,22 @@
 //this script replaces a string with another
 //currently does "a" to "b", will modify later to replace emojis with text
 
+//parse json emojis into variable
+var emojis;
+var req=new XMLHttpRequest();
+req.open('GET',chrome.extension.getURL('/scripts/emojiDict.json'),true);
+req.onload=function(){
+  if(req.readyState==4&&req.status==200){
+    emojis=JSON.parse(req.response);
+
+    var emoj='U+1F600';//example unicode point
+    console.log(emojis[emoj].name);//retrieves corresponding emoji description
+
+  }
+}
+req.send();
+//console.log(emojis);
+
 //get all elements in webpage
 var elements=document.getElementsByTagName('*');
 
