b78ca2d6e52de3667ab74473d9c9358eb053e96e
Jennifer Du <jenniferdu97@gmail.com>
2020-03-04 15:55:26 +0000

    Now remembers if button is pressed and auto translates on page load if mode=true. (Need to fix so that it's independent per tab/domain)

--- popup/popup.js
+++ popup/popup.js
@@ -1,7 +1,26 @@
 //this script controls what happens when popup is interacted with
+window.onload=function(){
+  var modeToggle=document.querySelector('input[name=modeToggle]');
+  modeToggle.addEventListener('change',toggleMode);
 
-//toggle mode
-var modeToggle=document.querySelector('input[name=modeToggle]');
+  //debug
+  chrome.storage.local.get('manualTranslate',function(value){
+    console.log('currently: ',value.manualTranslate);
+  });
+
+  chrome.storage.local.get('manualTranslate',function(value){
+    val=value.manualTranslate;
+    if(value.manualTranslate==true){
+      console.log('it true');  //debug
+      $('#modeToggle').attr('checked',true);
+    }else{
+      console.log('it false');  //debug
+      $('#modeToggle').attr('checked', false);
+    }
+  });
+
+}
+
 function toggleMode(){
   if(modeToggle.checked){
     chrome.tabs.query({active:true,currentWindow:true},function(tabs){
@@ -10,7 +29,7 @@ function toggleMode(){
       );
     });
     chrome.storage.local.set({'manualTranslate':true},function(){});
-  }else{//translate back
+  }else{
     chrome.tabs.query({active:true,currentWindow:true},function(tabs){
       chrome.tabs.executeScript(
         tabs[0].id,{file:'/scripts/revert.js'}
@@ -18,10 +37,9 @@ function toggleMode(){
     });
     chrome.storage.local.set({'manualTranslate':false},function(){});
   }
-}
-modeToggle.addEventListener('change',toggleMode);
 
-$(document).ready(function(){
-  //for some reason code just doesn't wanna run here :))))
-  console.log('if u see dis in console it means it worky hehe');
-});
+  //debug
+  chrome.storage.local.get('manualTranslate',function(value){
+    console.log('now: ',value.manualTranslate);
+  });
+}
--- scripts/bg.js
+++ scripts/bg.js
@@ -15,3 +15,24 @@ chrome.runtime.onInstalled.addListener(function(){
   });
 
 });
+
+//check if mode is on, if so, translate on tab/page load
+chrome.tabs.onUpdated.addListener(function(tabId,changeInfo,tab){
+  if(changeInfo.status=='complete'&&tab.active){
+    chrome.storage.local.get('manualTranslate',function(value){
+      if(value.manualTranslate==true){
+        chrome.tabs.query({active:true,currentWindow:true},function(tabs){
+          chrome.tabs.executeScript(
+            tabs[0].id,{file:'/scripts/replace.js'}
+          );
+        });
+      }else{
+        chrome.tabs.query({active:true,currentWindow:true},function(tabs){
+          chrome.tabs.executeScript(
+            tabs[0].id,{file:'/scripts/revert.js'}
+          );
+        });
+      }
+    });
+  }
+});
