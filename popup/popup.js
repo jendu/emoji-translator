76937a6ec9be3a73535446c7868edaf390881c63
Jennifer Du <jenniferdu97@gmail.com>
2020-03-09 22:26:25 +0000

    Improved code.

--- popup/popup.js
+++ popup/popup.js
@@ -1,18 +1,6 @@
 //this script controls what happens when popup is interacted with
-//when popup loads...
+//whenever popup loads up...
 window.onload=function(){
-  //keep button on if already on, else off
-  chrome.storage.local.get('autoTranslate',function(value){
-    if(value.autoTranslate==true){
-      $('#autoButton').attr('checked',true);
-      $('#manualButton').attr('disabled',true);
-      $('.switch.manual').css('opacity','0.2');
-    }else{
-      $('#autoButton').attr('checked', false);
-      $('#manualButton').attr('disabled',false);
-    }
-  });
-
   //for novelty, displays random emoji and its corresponding description!
   $.getJSON(chrome.runtime.getURL('/scripts/emojiDict.json'),function(responseText) {
       var emojis=responseText;
@@ -25,20 +13,38 @@ window.onload=function(){
       $('#manualButton').on('change',toggleManual);
   });
 
-  //change bg colour
+  //auto-translate: keep button on if already on, else off
+  chrome.storage.local.get('autoTranslate',function(value){
+    if(value.autoTranslate==true){
+      $('#autoButton').attr('checked',true);
+      $('#manualButton').attr('disabled',true);
+      $('.switch.manual').css('opacity','0.2');
+    }else{
+      $('#autoButton').attr('checked', false);
+      $('#manualButton').attr('disabled',false);
+    }
+  });
+
+  //bg colour:
   $('.jscolor').on('change paste keyup',changeBGColour);
   chrome.storage.local.get('bgColour',function(value){
     if(typeof value.bgColour!='undefined'){
       $('.jscolor').css('background-color',value.bgColour);
       $('.jscolor').val((value.bgColour).substring(1));
+    }else{
+      var bgColour;
+      var code='bgColour=$(\'body\').css(\'background-color\');';
+      chrome.tabs.executeScript({code: code},
+        function(colour){
+          bgColour=colour;console.log(bgColour);
+        }
+      );
     }
   });
 
   $('.resetBGColour').on('click',function(){
     chrome.storage.local.remove('bgColour',function(){});
-    chrome.tabs.query({active:true,currentWindow:true},function(tabs){
-      chrome.tabs.reload(tabs[0].id);
-    });
+    chrome.tabs.reload();
     window.location.reload();
   });
 }
@@ -52,52 +58,31 @@ function rgb2hex(rgb){
   ("0" + parseInt(rgb[3],10).toString(16)).slice(-2) : '';
 }
 
-//translates/reverts when button is clicked
+//toggles auto translate mode
 function toggleAuto(){
-  if(autoButton.checked){
-    chrome.tabs.query({active:true,currentWindow:true},function(tabs){
-      chrome.tabs.executeScript(
-        tabs[0].id,{file:'/scripts/replace.js'}
-      );
-    });
-    chrome.storage.local.set({'autoTranslate':true},function(){});
-    $('#manualButton').attr('disabled',true);
-    $('.switch.manual').css('opacity','0.2');
-  }else{
-    chrome.tabs.query({active:true,currentWindow:true},function(tabs){
-      chrome.tabs.executeScript(
-        tabs[0].id,{file:'/scripts/revert.js'}
-      );
-    });
-    chrome.storage.local.set({'autoTranslate':false},function(){});
-    $('#manualButton').attr('disabled',false);
-    $('.switch.manual').css('opacity','1');
-  }
+  chrome.tabs.query({active:true,currentWindow:true},function(tabs){
+    if(autoButton.checked){
+      chrome.runtime.sendMessage({action:'translate'},function(){});
+      $('#manualButton').attr('disabled',true);
+      $('.switch.manual').css('opacity','0.2');
+    }else{
+      chrome.runtime.sendMessage({action:'revert'},function(){});
+      $('#manualButton').attr('disabled',false);
+      $('.switch.manual').css('opacity','1');
+    }
+  });
 }
 
+//translates/reverts on click
 function toggleManual(){
   if(manualButton.checked){
-    chrome.tabs.query({active:true,currentWindow:true},function(tabs){
-      chrome.tabs.executeScript(
-        tabs[0].id,{file:'/scripts/replace.js'}
-      );
-    });
-  }else{
-    chrome.tabs.query({active:true,currentWindow:true},function(tabs){
-      chrome.tabs.executeScript(
-        tabs[0].id,{file:'/scripts/revert.js'}
-      );
-    });
-  }
+      chrome.tabs.executeScript({file:'/scripts/replace.js'});
+  }else{chrome.tabs.executeScript({file:'/scripts/revert.js'});}
 }
 
 function changeBGColour(){
   var bgColour="#"+$('.jscolor').val();
-  chrome.tabs.query({active:true,currentWindow:true},function(tabs){
-    chrome.tabs.executeScript(
-      tabs[0].id,{file:'/scripts/changebgcolour.js'}
-    );
-  });
+  chrome.tabs.executeScript({file:'/scripts/changebgcolour.js'});
   chrome.storage.local.set({'bgColour':bgColour},function(){});
   console.log('saved colour ',bgColour);
 }
--- scripts/bg.js
+++ scripts/bg.js
@@ -9,39 +9,44 @@ chrome.runtime.onInstalled.addListener(function(){
     ],
     actions:[new chrome.declarativeContent.ShowPageAction()]
   };
-
   chrome.declarativeContent.onPageChanged.removeRules(undefined,function(){
     chrome.declarativeContent.onPageChanged.addRules([checkPageValid]);
   });
-
 });
 
-//check if button is on, if so, auto-translate on tab/page load
+//check if button is on, if so, auto-translate on tab/page load (message,sender,reply)
 chrome.tabs.onUpdated.addListener(function(tabId,changeInfo,tab){
   if(changeInfo.status=='complete'&&tab.active){
     chrome.storage.local.get('autoTranslate',function(value){
       if(typeof value.autoTranslate!='undefined'){
         if(value.autoTranslate==true){
           chrome.tabs.query({active:true,currentWindow:true},function(tabs){
-            chrome.tabs.executeScript(
-              tabs[0].id,{file:'/scripts/replace.js'},_=>chrome.runtime.lastError
-            );
+            chrome.tabs.executeScript(tabs[0].id,{file:'/scripts/replace.js'},_=>chrome.runtime.lastError);
           });
         }
       }
     });
-
     chrome.storage.local.get('bgColour',function(value){
       if(typeof value.bgColour!='undefined'){
         var bgColour=value.bgColour;
         console.log(bgColour);
         chrome.tabs.query({active:true,currentWindow:true},function(tabs){
-          chrome.tabs.executeScript(
-            tabs[0].id,{file:'/scripts/changebgcolour.js'},_=>chrome.runtime.lastError
-          );
+          chrome.tabs.executeScript(tabs[0].id,{file:'/scripts/changebgcolour.js'},_=>chrome.runtime.lastError);
         });
       }
     });
 
   }
+});
+
+chrome.runtime.onMessage.addListener(function(message){
+  console.log('test ',message);
+  if(message.action=='translate'){
+    chrome.tabs.executeScript({file:'/scripts/replace.js'},_=>chrome.runtime.lastError);
+    chrome.storage.local.set({'autoTranslate':true},function(){});
+  }else if(message.action=='revert'){
+    chrome.tabs.executeScript({file:'/scripts/revert.js'},_=>chrome.runtime.lastError);
+    chrome.storage.local.set({'autoTranslate':false},function(){});
+  }
+  return true;
 });
